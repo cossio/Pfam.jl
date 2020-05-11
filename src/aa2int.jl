5b5a4512be9c501c157e0de9e296ca013c0176c5
cossio <j.cossio.diaz@gmail.com>
2020-05-12 00:30:56 +0200

    aa2int with Int8

--- src/aa2int.jl
+++ src/aa2int.jl
@@ -7,11 +7,11 @@ Amino-acid one-letter code to integer (1 to 21, with gap = 21).
 """
 function aa2int(c::Union{Char,UInt8})
     # from: https://github.com/carlobaldassi/GaussDCA.jl
-    alphabet = (1,21, 2, 3, 4, 5, 6, 7, 8,21, 9,10,11,12,21,13,14,15,16,17,21,18,19,21,20)
+    alphabet = Int8.((1,21, 2, 3, 4, 5, 6, 7, 8,21, 9,10,11,12,21,13,14,15,16,17,21,18,19,21,20))
               # A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y
     i = UInt8(c) - 0x40
     1 <= i <= 25 && return alphabet[i]
-    return 21
+    return Int8(21)
 end
 
 """
