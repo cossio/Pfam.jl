3b4a8182275877eb4216655b28c502a8956f203a
cossio <j.cossio.diaz@gmail.com>
2021-12-29 00:27:41 +0100

    fix example

--- docs/src/literate/PF00011.jl
+++ docs/src/literate/PF00011.jl
@@ -2,6 +2,7 @@
 Load the PF00011 family from PFAM.
 =#
 
-using Pfam
-msa = Pfam.MSA("PF00011")  ## describe the requested MSA
-df = Pfam.load(msa) ## load as a DataFrame
+import Pfam
+msa = Pfam.MSA("PF00011");  ## describe the requested MSA
+df = Pfam.load(msa); ## load as a DataFrame
+@show df[1:50,:]; ## show some rows
